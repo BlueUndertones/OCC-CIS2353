86f1ed9ef455a921b15baf19ae79b6a59326e458
d3mario <iid3mario@gmail.com>
2014-11-14 19:11:56 -0500

    Party Class

---
+++ Party.java
@@ -0,0 +1,38 @@
+package adventureGame;
+
+public class Party
+{
+
+
+
+	public Party(Character[] character ) //demario
+	{
+		
+	}
+	
+	public static void sleep()//demario
+	{
+		
+	}
+
+
+	public static void fight() //krupa
+	{
+		
+	}
+	
+	public static void search() // Heather
+	{
+		 
+	}
+	
+	public static void hide()// nadir
+	{
+		
+	}
+	
+	public static void run()//demario
+	{
+		
+	}
+}
