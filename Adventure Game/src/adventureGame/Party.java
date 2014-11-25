220be651f0a8ea1890335a435d12caa0d887adfd
nadhir-falta <nxfalta@oaklandcc.edu>
2014-11-24 23:21:47 -0500

    changes to Search method

--- Adventure Game/src/adventureGame/Party.java
+++ Adventure Game/src/adventureGame/Party.java
@@ -7,7 +7,7 @@ public class Party
    static Sequence party;
    private int MIN_PARTY_SIZE = 0;
    private int MAX_PARTY_SIZE = 6;
-   private boolean isSearched = false;
+   private static boolean isSearched = false;
    
 	public Party(  Sequence party  ) //demario
 	{
@@ -54,36 +54,39 @@ public class Party
 	
 	public static void search(Room room, Player player) // Heather
 	{
-		//check player's int
-	      // you can only search a room the first time you're visiting it.  After that, no searching.
-	      if (room.isVisited() == true || isSearched == true) {
-	        System.out.println("This room has already been searched.");
-	        return;
-	      } 
-	
-	      //playerGold = player.getGold();       
-	      int playerInt = player.getIntelligence();
-	      Die intDie = new Die(20);
-	      int roomInt = intDie.roll();
-	      if (playerInt > roomInt) { // ? what about >= ?
-	        int goldFound = room.getGold();
-	        System.out.println(player.getName()  + " has found " + goldFound + " gold pieces.");
-	        //gold = playerGold + goldFound;
-	        player.setGold(player.getGold() + goldFound);
-	        //player.addGold(gold);
-	      }
-	      else {
-	        //return nothing found..
-	        System.out.println("There is nothing to find here.");
-	      }
-	
-	      isSearched = true;
-	    }
+		// check player's int
+		// you can only search a room the first time you're visiting it. After
+		// that, no searching.
+//		if (room.isVisited() == true || isSearched == true) {
+//			System.out.println("This room has already been searched.");
+//			return;
+//		}
+
+		// playerGold = player.getGold();
+		int playerInt = player.getIntelligence();
+		Die intDie = new Die(20);
+		int roomInt = intDie.roll();
+		if (playerInt > roomInt) { // ? what about >= ?
+			int goldFound = room.getGold();
+			System.out.println(player.getName() + " has found " + goldFound + " gold pieces.");
+			// gold = playerGold + goldFound;
+			player.setGold(player.getGold()+ goldFound);
+//			System.out.println("room gold is " + roomInt);
+//			System.out.println("intele is " + player.getIntelligence());
+//			System.out.println(player.getName() + "  " + player.getStrength());
+			// player.addGold(gold);
+		} else {
+			// return nothing found..
+//			System.out.println("room gold is " +roomInt);
+//			System.out.println("intele is " + player.getIntelligence());
+			System.out.println("There is nothing to find here.");
+		}
+		isSearched = true;
 	}
 	
 	public static void hide()// nadir
 	{
-		
+		//this method exists in the board class
 	}
 	
 	public static void run()//demario
