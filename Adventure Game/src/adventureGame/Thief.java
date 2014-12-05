cdfe929744396278db3d2c8a2e48175b47c8db5e
d3mario <iid3mario@gmail.com>
2014-12-05 01:27:57 -0500

    Wizard, Thief, Warrior Classes
    Uploaded + Completed Modified Player Classes

--- Adventure Game/src/adventureGame/Thief.java
+++ Adventure Game/src/adventureGame/Thief.java
@@ -10,26 +10,27 @@ package adventureGame;
  *
  * @author DeMario
  */
-public class Wizard extends Player implements character
+public class Thief extends Player implements character
 {
     private final int NUMBER_OF_ROLLS = 6; 
     private final int NUMBER_OF_SIDES = 3;   
     private final int PLUS_ONE = 1;
     private final int MINUS_ONE = -1;
     
-    public Wizard( String name )
+    public Thief( String name )
     {
         super( name ); 
-        this.setStrength ( numberOfDiceRolls(NUMBER_OF_ROLLS, NUMBER_OF_SIDES) + MINUS_ONE );
+        this.setStrength ( numberOfDiceRolls(NUMBER_OF_ROLLS, NUMBER_OF_SIDES) + PLUS_ONE );
         this.setDexterity ( numberOfDiceRolls( NUMBER_OF_ROLLS, NUMBER_OF_SIDES ) );
-        this.setIntelligence ( numberOfDiceRolls( NUMBER_OF_ROLLS, NUMBER_OF_SIDES ) + PLUS_ONE);
-        this.setHitPoints ( numberOfDiceRolls(NUMBER_OF_ROLLS, NUMBER_OF_SIDES ) + MINUS_ONE );
+        this.setIntelligence ( numberOfDiceRolls( NUMBER_OF_ROLLS, NUMBER_OF_SIDES ) + MINUS_ONE);
+        this.setHitPoints ( numberOfDiceRolls(NUMBER_OF_ROLLS, NUMBER_OF_SIDES ) + PLUS_ONE );
     }
     
+	
 
     public String toString()
     {
-        String stats = "<html>Wizard: " + this.getName() +
+        String stats = "<html>Thief: " + this.getName() +
                         " \n<br>Hit Points: " + this.getHitPoints()+
                         " \n<br>Intelligence: " + this.getIntelligence() +
                         " \n<br>Dexterity: " + this.getDexterity() + 
