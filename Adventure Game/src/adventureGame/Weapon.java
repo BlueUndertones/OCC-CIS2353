c5da48a1dc5866a557ec336b5c9a69010f4cc2ab
nadhir-falta <nxfalta@oaklandcc.edu>
2014-12-06 19:32:31 -0500

    created method get name for the weapon

--- Adventure Game/src/adventureGame/Weapon.java
+++ Adventure Game/src/adventureGame/Weapon.java
@@ -3,13 +3,17 @@ public class Weapon extends Items {
 
 	private int damage = 0;
 	private int durability = 0;
-
+	private String name  ="";
 	public Weapon(String name, String desc, int dmg, int dur) {
 		super(name, desc);
+		this.name = name;
 		setDamage(dmg+1);
 		setDurability(dur);
 	}
-
+	public String getName ()
+	{
+		return this.name;
+	}
 	public int getDamage() {
 		return damage;
 	}
